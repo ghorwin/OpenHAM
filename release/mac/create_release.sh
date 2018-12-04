7452e83e4e3d59fe03343f02f87d64492dd0a2df
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2018-12-04 20:14:39 +0100

    Added release creation script for mac.

---
+++ release/mac/create_release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script to create a release of OpenHAM for Linux
+
+VERSION=0.3
+DISTRO=Darwin-10.11.6
+
+cd ../../build/cmake && 
+rm -rf bb-gcc && 
+./build.sh 2 release skip-test &&
+cd - &&
+rm -rf OpenHAM-$VERSION-$DISTRO &&
+mkdir OpenHAM-$VERSION-$DISTRO &&
+cd OpenHAM-$VERSION-$DISTRO &&
+cp ../../../bin/release/OpenHAMSolver . &&
+cp ../../../LICENSE . &&
+cp ../../../AUTHORS . &&
+cp -r ../../../data/examples . &&
+cd .. && 
+7za a -t7z OpenHAM-$VERSION-$DISTRO.7z OpenHAM-$VERSION-$DISTRO/* &&
+
+echo '*** Created release for version '$VERSION' ***'
+
+
