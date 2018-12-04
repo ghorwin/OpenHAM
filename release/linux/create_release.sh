1961c887097c107c33ba2f6ffc3bf0b615c627f8
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2018-12-04 15:38:09 +0100

    Added AUTHORS file.

--- release/linux/create_release.sh
+++ release/linux/create_release.sh
@@ -2,27 +2,22 @@
 
 # Script to create a release of OpenHAM for Linux
 
-# build solver
-
 VERSION=0.3
 
 cd ../../build/cmake && 
-#rm -rf bb-gcc && 
-#./build.sh 2 release &&
+rm -rf bb-gcc && 
+./build.sh 2 release skip-test &&
 cd - &&
 rm -rf OpenHam-$VERSION &&
 mkdir OpenHam-$VERSION &&
 cd OpenHam-$VERSION &&
 cp ../../../bin/release/OpenHAMSolver . &&
-mkdir examples &&
-cp -r ../../../data examples/ &&
-cd .. &&
+cp ../../../LICENSE . &&
+cp ../../../AUTHORS . &&
+cp -r ../../../data/examples . &&
+cd .. && 
+7za a -t7z OpenHam-$VERSION.7z OpenHam-$VERSION/* &&
 
 echo '*** Created release for version '$VERSION' ***'
-
-
-
-
-
 
 
