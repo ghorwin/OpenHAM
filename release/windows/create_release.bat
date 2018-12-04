dae3b5f481c04cb8fee794f8321e7778db5524ae
Andreas Nicolai <andreas.nicolai@gmx.net>
2018-12-04 16:11:35 +0100

    Added release script to create Windows releases.

---
+++ release/windows/create_release.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set RELDIR=OpenHAM-03-Win64
+
+cd ..\..\build\cmake
+call build_VC_x64.bat
+cd ..\..\release\windows
+mkdir %RELDIR%
+copy ..\..\bin\release_x64\OpenHAMSolver.exe %RELDIR%\
+copy VC14_x64_DLLs\* %RELDIR%\
+copy ..\..\LICENSE %RELDIR%\
+copy ..\..\AUTHORS %RELDIR%\
+mkdir %RELDIR%\examples
+xcopy /y /s /e ..\..\data\examples %RELDIR%\examples
+
