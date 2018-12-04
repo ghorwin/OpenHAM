5961100f059d7f5e82d432e4b72431c7a41fa961
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2018-12-04 15:42:52 +0100

    Minor tweaks to release building script.

--- release/linux/create_release.sh
+++ release/linux/create_release.sh
@@ -3,20 +3,21 @@
 # Script to create a release of OpenHAM for Linux
 
 VERSION=0.3
+DISTRO=Ubuntu-18.04
 
 cd ../../build/cmake && 
 rm -rf bb-gcc && 
 ./build.sh 2 release skip-test &&
 cd - &&
-rm -rf OpenHam-$VERSION &&
-mkdir OpenHam-$VERSION &&
-cd OpenHam-$VERSION &&
+rm -rf OpenHAM-$VERSION-$DISTRO &&
+mkdir OpenHAM-$VERSION-$DISTRO &&
+cd OpenHAM-$VERSION-$DISTRO &&
 cp ../../../bin/release/OpenHAMSolver . &&
 cp ../../../LICENSE . &&
 cp ../../../AUTHORS . &&
 cp -r ../../../data/examples . &&
 cd .. && 
-7za a -t7z OpenHam-$VERSION.7z OpenHam-$VERSION/* &&
+7za a -t7z OpenHAM-$VERSION-$DISTRO.7z OpenHAM-$VERSION-$DISTRO/* &&
 
 echo '*** Created release for version '$VERSION' ***'
 
